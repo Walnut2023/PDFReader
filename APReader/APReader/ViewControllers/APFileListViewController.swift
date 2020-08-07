cfa1bc227c9ff724c8af32bc84fac295fab3d066
Tango <csqingyang@foxmail.com>
2020-08-07 21:26:23 +0800

    adjust preview view layout step 01

--- APReader/APReader/ViewControllers/APFileListViewController.swift
+++ APReader/APReader/ViewControllers/APFileListViewController.swift
@@ -23,12 +23,7 @@ class APFileListViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.isHidden = false
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.isHidden = true
+        navigationController?.hidesBarsOnTap = false
     }
     
     func setupDataSource() {
--- APReader/APReader/ViewControllers/APPreviewViewController.swift
+++ APReader/APReader/ViewControllers/APPreviewViewController.swift
@@ -19,17 +19,25 @@ class APPreviewViewController: UIViewController {
     @IBOutlet weak var thumbnailView: PDFThumbnailView!
     @IBOutlet weak var thumbnailViewContainer: UIView!
     @IBOutlet weak var pageControl: UIView!
-    @IBOutlet weak var topToolBar: UIToolbar!
     @IBOutlet weak var toolContainer: UIView!
-    @IBOutlet weak var topToolbarHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var pdfViewLeftMarginConstraint: NSLayoutConstraint!
-    
+        
+    private lazy var backBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "arrow_back"), style: .plain, target: self, action: #selector(backAction))
+    private lazy var outlineBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "outline"), style: .plain, target: self, action: #selector(outlineAction))
+    private lazy var editBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "edit"), style: .plain, target: self, action: #selector(editAction))
+    private lazy var edittingBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "editing"), style: .plain, target: self, action: #selector(editAction))
+    private lazy var bookmarkBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "bookmark"), style: .plain, target: self, action: #selector(bookmarkAction))
+    private lazy var searchBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "search"), style: .plain, target: self, action: #selector(searchAction))
+    private lazy var undoPreviousBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "undopre"), style: .plain, target: self, action: #selector(undoLastAction))
+    private lazy var undoNextBarButtonItem = UIBarButtonItem(image: UIImage.init(named: "undonext"), style: .plain, target: self, action: #selector(undoNextAction))
+
     private lazy var tapFestureRecognizer = UITapGestureRecognizer()
     private lazy var pdfDrawingGestureRecognizer = APDrawingGestureRecognizer()
     private lazy var pdfTextDrawingGestureRecognizer = APTextDrawingGestureRecognizer()
     
     private var topbarActionControl: APPDFToolbarActionControl?
-
+    private var editButtonClicked: Bool = false
+    
     private let pdfDrawer = APPDFDrawer()
     private let pdfTextDrawer = APPDFTextDrawer()
     
@@ -42,14 +50,34 @@ class APPreviewViewController: UIViewController {
         setupUI()
         setupPDFView()
         loadPdfFile()
+
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        
+        navigationController?.hidesBarsOnTap = editButtonClicked ? false : true
+
+        if editButtonClicked {
+            return false
+        } else {
+            return navigationController?.isNavigationBarHidden == true
+        }
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.navigationBar.isHidden = true
         super.viewWillAppear(animated)
     }
     
     private func setupUI() {
+        navigationController?.hidesBarsOnTap = false
+
+        navigationItem.setLeftBarButtonItems([backBarButtonItem, outlineBarButtonItem], animated: true)
+        navigationItem.setRightBarButtonItems([bookmarkBarButtonItem, searchBarButtonItem, editBarButtonItem], animated: true)
+        
         self.pageControl.isHidden = true
         self.topbarActionControl = APPDFToolbarActionControl(pdfPreviewController: self)
         self.pdfViewLeftMarginConstraint.constant = 120
@@ -79,19 +107,70 @@ class APPreviewViewController: UIViewController {
         let pdfDocument = PDFDocument(url: fileURL)
         pdfView.document = pdfDocument
         self.pdfDocument = pdfDocument
-        pdfTittleLabel.text = self.filePath
     }
     
     // MARK: -  Action
     
     @objc func tappedAction() {
         print("tapped")
-        UIView.transition(with: self.topToolBar, duration: 0.25, options: .transitionCrossDissolve, animations: {
-            self.topToolBar.isHidden = !self.topToolBar.isHidden
+        UIView.transition(with: self.toolContainer, duration: 0.25, options: .transitionCrossDissolve, animations: {
+            self.navigationController?.setNavigationBarHidden(!(self.navigationController?.isNavigationBarHidden ?? false) , animated: true)
             self.toolContainer.isHidden = !self.toolContainer.isHidden
             self.thumbnailView.isHidden = !self.thumbnailView.isHidden
             self.pdfViewLeftMarginConstraint.constant = self.pdfViewLeftMarginConstraint.constant > 0 ? 0 : 120
         }, completion: nil)
+    }
+    
+    @objc func backAction(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func outlineAction(_ sender: Any) {
+        print("Click outline")
+        self.topbarActionControl?.showOutlineTableForPFDDocument(for: self.pdfDocument, from: sender)
+    }
+
+    @objc func editAction() {
+        print("editAction tapped")
+        editButtonClicked = !editButtonClicked
+        if editButtonClicked {
+            navigationItem.setLeftBarButtonItems([backBarButtonItem], animated: true)
+            navigationItem.setRightBarButtonItems([bookmarkBarButtonItem, searchBarButtonItem, edittingBarButtonItem, undoNextBarButtonItem, undoPreviousBarButtonItem], animated: true)
+            
+            self.pageControl.isHidden = false
+            pdfView.removeGestureRecognizer(self.tapFestureRecognizer)
+            self.pdfDrawingGestureRecognizer = APDrawingGestureRecognizer()
+            pdfView.addGestureRecognizer(pdfDrawingGestureRecognizer)
+            pdfDrawingGestureRecognizer.drawingDelegate = pdfDrawer
+
+        } else {
+            navigationItem.setLeftBarButtonItems([backBarButtonItem, outlineBarButtonItem], animated: true)
+            navigationItem.setRightBarButtonItems([bookmarkBarButtonItem, searchBarButtonItem, editBarButtonItem], animated: true)
+            
+            self.pageControl.isHidden = true
+            pdfView.removeGestureRecognizer(self.pdfDrawingGestureRecognizer)
+            self.tapFestureRecognizer = UITapGestureRecognizer()
+            tapFestureRecognizer.addTarget(self, action: #selector(tappedAction))
+            pdfView.addGestureRecognizer(tapFestureRecognizer)
+        }
+    }
+    
+    @objc func bookmarkAction(_ sender: Any) {
+        print("Click bookmark")
+        self.topbarActionControl?.showBookmarkTable(from: sender)
+    }
+    
+    @objc func searchAction(_ sender: Any) {
+        print("click search")
+        self.topbarActionControl?.showSearchViewController(for: self.pdfDocument, from: sender)
+    }
+    
+    @objc func undoLastAction() {
+        print("redo last action tapped")
+    }
+    
+    @objc func undoNextAction() {
+        print("redo next action tapped")
     }
     
     @IBAction func annotateAction(_ sender: Any) {
@@ -130,11 +209,6 @@ class APPreviewViewController: UIViewController {
         }
     }
     
-    @IBAction func saveAction(_ sender: Any) {
-        let path = Bundle.main.url(forResource: filePath, withExtension: "pdf")
-        pdfView.document?.write(to: path!)
-    }
-    
     @IBAction func pageUpAction(_ sender: Any) {
         print("page up action")
         pdfView.goToPreviousPage(sender)
@@ -153,25 +227,6 @@ class APPreviewViewController: UIViewController {
     @IBAction func switchPenColor(_ sender: Any) {
         print("switchPenColor")
         self.pdfDrawer.color = .blue
-    }
-    
-    @IBAction func backAction(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-    @IBAction func outlineClickAction(_ sender: Any) {
-        print("Click outline")
-        self.topbarActionControl?.showOutlineTableForPFDDocument(for: self.pdfDocument, from: sender)
-    }
-    
-    @IBAction func bookmarkAction(_ sender: Any) {
-        print("Click bookmark")
-        self.topbarActionControl?.showBookmarkTable(from: sender)
-    }
-    
-    @IBAction func searchAction(_ sender: Any) {
-        print("click search")
-        self.topbarActionControl?.showSearchViewController(for: self.pdfDocument, from: sender)
     }
     
     func didSelectPdfOutline(_ pdfOutline: PDFOutline?) {
